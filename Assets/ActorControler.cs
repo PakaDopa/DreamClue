5c78cfc7962f8cc0c2d1be68ede28bf9f3ad36bb
PakaDopa <detols33@naver.com>
2024-05-25 23:11:43 +0900

    update text system

--- Assets/ActorControler.cs
+++ Assets/ActorControler.cs
@@ -1,7 +1,15 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Utility;
+
+public enum ActorState
+{
+    TEXTING, // 출력중
+    TEXT,    // 출력완료
+    NONE,    // 
+}
 
 public class ActorControler : MonoBehaviour
 {
@@ -11,11 +19,13 @@ public class ActorControler : MonoBehaviour
     [Header("UI Component")]
     public TMP_Text _textBox;
     public TMP_Text _nameTextbox;
-
+    
     [SerializeField]
     public List<string> _currentText;
+    private int _index = 0;
+    public ActorState _state = ActorState.NONE;
 
-    private void Start()
+    private void Awake()
     {
         _currentText = new List<string>();
 
@@ -24,26 +34,44 @@ public class ActorControler : MonoBehaviour
         if (_nameTextbox != null)
             _nameTextbox.text = _actorName;
     }
-
     private void ShowText(EVENT_TYPE eventType, Component Sender, TransformEventArgs args = null)
     {
         // 0 index -> actorType
         // 1 index -> text
         if (args.value.Length <= 0)
             return;
+
+        var bnt = GetComponent<Button>();
+        bnt.enabled = false;
+        _textBox.enabled = false;
         if (args.value[0].ToString() != _actorName)
+        {
+            // 내가 해당되는 버튼이 아닌거임 =ㅅ=
             return;
-        //CustomDebug.Log("Find Object!");
+        }
+        _textBox.enabled = true;
+        bnt.enabled = true;
+        //_state = ActorState.TEXT;
+        CustomDebug.Log("!! Show text " + _actorName);
+        //초기화
         _currentText.Clear();
         _currentText = (List<string>)args.value[1];
+        _index = 0;
 
-        _textBox.text = _currentText[0];
+        ClickBnt();
     }
 
-    private void OnMouseDown()
+    public void ClickBnt()
     {
-        if (_actorName == "null")
-            return;
-        Debug.Log("Click!");
+        if (_currentText.Count <= _index || _currentText[_index] == "") //다음 Did로 넘어가야함/
+        {
+            //_textBox.enabled = false;
+            EventManager.Instance.PostNotification(EVENT_TYPE.UPDATE_SUB_INDEX, this, new TransformEventArgs(null));
+        }
+        else
+        {
+            _textBox.text = _currentText[_index].ToString();
+            _index++;
+        }
     }
 }
--- Assets/Resources/Scripts/Manager/GameControler.cs
+++ Assets/Resources/Scripts/Manager/GameControler.cs
@@ -1,33 +1,37 @@
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 using Utility;
 
 public class GameControler : MonoBehaviour
 {
-    [SerializeField] private string _startMidName = "";
-
     List<MainFlowData> _currentMain;
     int _currentMainIndex = 0;
 
     List<SubFlowData> _currentSub;
     int _currentSubIndex = 0;
 
+    Dictionary<string, List<MainFlowData>> _datas;
+    List<string> _keys;
+
     void Start()
     {
-        EventManager.Instance.AddListener(EVENT_TYPE.UPDATE_MAIN_INDEX, UpdateMainIndex);
+        //EventManager.Instance.AddListener(EVENT_TYPE.UPDATE_MAIN_INDEX, UpdateMainIndex);
         EventManager.Instance.AddListener(EVENT_TYPE.UPDATE_SUB_INDEX, UpdateSubIndex);
-        EventManager.Instance.AddListener(EVENT_TYPE.COMPLETE_MAIN_EVENT, EndMain);
-        EventManager.Instance.AddListener(EVENT_TYPE.COMPLETE_SUB_EVENT, EndSub);
+        //EventManager.Instance.AddListener(EVENT_TYPE.COMPLETE_MAIN_EVENT, EndMain);
+        //EventManager.Instance.AddListener(EVENT_TYPE.COMPLETE_SUB_EVENT, EndSub);
 
-        Dictionary<string, List<MainFlowData>> datas = GameManager.Instance.GetMainFlowData();
-        _currentMain = datas.First().Value;
+        //초기 Init
+        _datas = GameManager.Instance.GetMainFlowData();
+        _keys = _datas.Keys.ToList();
 
-        _currentSub = GameManager.Instance.GetSubFlowData(_currentMain[_currentMainIndex].dialogueId);
+        // 공통되는 스크립트 파트
+        _currentMain = _datas[_keys[_currentMainIndex]];
+        _currentSub = GameManager.Instance.GetSubFlowData(_currentMain[0].dialogueId);
         string name = _currentSub[_currentSubIndex].actorName;
         List<string> contexts = _currentSub[_currentSubIndex].contexts.ToList();
 
-        CustomDebug.Log(string.Format("{0}  {1}", name, contexts[0]));  
         EventManager.Instance.PostNotification(EVENT_TYPE.SHOW_TEXT,
             this,
             new TransformEventArgs(null, name, contexts));
@@ -40,16 +44,46 @@ public class GameControler : MonoBehaviour
 
     void UpdateSubIndex(EVENT_TYPE eventType, Component Sender, TransformEventArgs args = null)
     {
-
+        _currentSubIndex++;
+        //CustomDebug.Log("   _currentSubIndex " + _currentSubIndex);
+        if (_currentSub.Count <= _currentSubIndex)
+            EndSub();
+        else
+        {
+            string name = _currentSub[_currentSubIndex].actorName;
+            List<string> contexts = _currentSub[_currentSubIndex].contexts.ToList();
+            EventManager.Instance.PostNotification(EVENT_TYPE.SHOW_TEXT,
+                this,
+                new TransformEventArgs(null, name, contexts));
+        }
     }
 
-    void EndSub(EVENT_TYPE eventType, Component Sender, TransformEventArgs args = null)
+    //Sub이 끝낫다는 것은 Main이 하나 증가한다는 뜻
+    void EndSub()
     {
+        _currentMainIndex++;
+        if (_currentMainIndex >= _keys.Count)
+            EndMain();
+        
+        string key = _keys[_currentMainIndex];
+        _currentMain = _datas[_keys[_currentMainIndex]];
 
+        if (_currentMain[0].isDream)
+            CustomDebug.Log(" Dream 이벤트입니다.");
+
+        _currentSub = GameManager.Instance.GetSubFlowData(_currentMain[0].dialogueId);
+        _currentSubIndex = 0;
+
+        string name = _currentSub[_currentSubIndex].actorName;
+        List<string> contexts = _currentSub[_currentSubIndex].contexts.ToList();
+  
+        EventManager.Instance.PostNotification(EVENT_TYPE.SHOW_TEXT,
+            this,
+            new TransformEventArgs(null, name, contexts));
     }
-
-    void EndMain(EVENT_TYPE eventType, Component Sender, TransformEventArgs args = null)
+    //게임 종료
+    void EndMain()
     {
-
+        CustomDebug.Log("게임 끝!!");
     }
 }
